cf47aec46f2e112b1270410ba54019485dcadca9
Anastasia Tulupnikoff <anastasiatulupnikoff@Anastasias-MacBook-Air.local>
2025-01-14 10:31:41 -0500

    first commit

--- Tariff-plan-app/src/App.jsx
+++ Tariff-plan-app/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Tariff from './components/Tariff/Tariff';
+import './App.css'; 
+
+const tariffs = [
+  { title: 'Безлимитный 300', price: 300, speed: 10, backgroundColor: '#17a2b8' }, // Бирюзовый
+  { title: 'Безлимитный 450', price: 450, speed: 50, backgroundColor: '#28a745' }, // Зеленый
+  { title: 'Безлимитный 550', price: 550, speed: 100, backgroundColor: '#dc3545' }, // Красный (выделенный)
+  { title: 'Безлимитный 1000', price: 1000, speed: 200, backgroundColor: '#343a40' }, // Черный
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="tariffs">
+          {tariffs.map((tariff, index) => (
+              <Tariff
+              key={index}
+              title={tariff.title}
+              price={tariff.price}
+              speed={tariff.speed}
+              isHighlighted={tariff.price === 550}
+              backgroundColor={tariff.backgroundColor} 
+              />
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
